41f417c41c9747ad6ca515494274b8ce2ad4fd88
ariploom <ari@ploom.com>
2014-02-06 15:56:03 -0800

    Built analyze raw data up to absorbance calculation. Created skeleton of calc_rg. Added directories for raw data and lookup tables.

---
+++ analyze_raw_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from numpy import *
+from calc_rg import *
+from matplotlib.pyplot import *
+
+def analyze_raw_data(experiment):
+	filename = 'raw_data/' + experiment + '.csv'
+	data = genfromtxt(filename, delimiter=',', dtype=None)
+	
+	puff_volume = data[0,0]
+	puff_duration = data[0,1]
+	num_puffs = data[0,2]
+
+	time = squeeze(data[1:len(data),0])
+	intensities = data[1:len(data),1:5]
+
+	puff_times = []
+	for t in range(len(time)):
+		if data[t,7] != 0:
+			puff_times.append(time[t])
+
+	absorbtions = empty_like(intensities)
+	wavelengths = array([550., 650., 750., 850.])
+	base_intensities = average(intensities[0:250, :], axis=0)
+	print base_intensities.shape
+
+
+	for t in range(len(time)):
+		for l in range(len(wavelengths)):		
+			absorbtions[t,l] = log(base_intensities[l]/intensities[t,l])
+
+	plot(time, absorbtions)
+	show()
+
+
+
+
+
+
+
+analyze_raw_data('A0004A')
---
+++ calc_rg.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def calc_rg(absorbtions):
+	rg = .5 # microns
+	return rg
